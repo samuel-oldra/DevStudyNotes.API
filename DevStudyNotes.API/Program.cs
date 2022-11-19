f89eb2899a336d56c78dbd482c0e6c785ebcf49c
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-11-19 00:00:00 -0300

    Padronização arquivo Program.cs

--- DevStudyNotes.API/Program.cs
+++ DevStudyNotes.API/Program.cs
@@ -7,52 +7,62 @@ using Serilog.Sinks.MSSqlServer;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DevStudyNotes");
 
-// Para publicar, descomente as três linhas abaixo, e comente as outras 3 linhas que configuram o EF Core para usar o SQL Server
-// builder.Services.AddDbContext<StudyNoteDbContext>(
-//     o => o.UseInMemoryDatabase("DevStudyNotesDb")
-// );
+// PARA ACESSO AO BANCO EM MEMÓRIA
+builder.Services.AddDbContext<StudyNoteDbContext>(o => o.UseInMemoryDatabase("DevStudyNotesDb"));
 
-builder.Services.AddDbContext<StudyNoteDbContext>(
-    o => o.UseSqlServer(connectionString)
-);
-
-// Para publicar, descomente as linhas abaixo relativas a configuração do Serilog
-builder.Host.ConfigureAppConfiguration((hostingContext, config) => {
-    Serilog.Log.Logger = new LoggerConfiguration()
-        .Enrich.FromLogContext()
-        .WriteTo.MSSqlServer(connectionString,
-            sinkOptions: new MSSqlServerSinkOptions() {
-                AutoCreateSqlTable = true,
-                TableName = "Logs"
-            })
-        .WriteTo.Console()
-        .CreateLogger();
-}).UseSerilog();
+// PARA ACESSO AO SQL Server
+// var connectionString = builder.Configuration.GetConnectionString("DevStudyNotes");
+// builder.Services.AddDbContext<StudyNoteDbContext>(o => o.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo {
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo
+    {
         Title = "DevStudyNotes.API",
         Version = "v1",
-        Contact = new OpenApiContact {
-            Name = "LuisDev",
-            Email = "contato@ld.com.br",
-            Url = new Uri("https://luisdev.com.br")
+        Contact = new OpenApiContact
+        {
+            Name = "Samuel B. Oldra",
+            Email = "samuel.oldra@gmail.com",
+            Url = new Uri("https://github.com/samuel-oldra")
         }
     });
 
     var xmlFile = "DevStudyNotes.API.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
+    o.IncludeXmlComments(xmlPath);
 });
+
+// Serilog
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    Serilog.Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+
+        // PARA LOG NO SQL Server
+        // .WriteTo.MSSqlServer(
+        //     connectionString,
+        //     sinkOptions: new MSSqlServerSinkOptions()
+        //     {
+        //         AutoCreateSqlTable = true,
+        //         TableName = "Logs"
+        //     })
+
+        // PARA LOG NO CONSOLE
+        .WriteTo.Console()
+
+        .CreateLogger();
+}).UseSerilog();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// INFO: Swagger visível só em desenvolvimento
 if (true)
 {
     app.UseSwagger();
